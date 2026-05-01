86160ebed62bdb56d2617b5f0d97fcccb428cee4
Princeton Afeez <princetonafeez@gmail.com>
2026-05-01 13:22:22 -0700

    A dimension is a vector of exponents for the base dimensions

--- src/strictunits/dimensions.py
+++ src/strictunits/dimensions.py
@@ -7,3 +7,19 @@ from typing import ClassVar, Iterable
 BASE_DIMENSION_COUNT = 7
 BASE_DIMENSION_LABELS = ("L", "M", "T", "I", "Theta", "N", "J")
 
+
+@dataclass(frozen=True)
+class Dimension:
+    exponents: tuple[int, ...]
+    
+    def __init__(self, exponents: tuple[int, ...]) -> None:
+        self.exponents = exponents
+
+    NONE: ClassVar["Dimension"]
+    LENGTH: ClassVar["Dimension"]
+    MASS: ClassVar["Dimension"]
+    TIME: ClassVar["Dimension"]
+    CURRENT: ClassVar["Dimension"]
+    TEMPERATURE: ClassVar["Dimension"]
+    AMOUNT: ClassVar["Dimension"]
+    LUMINOSITY: ClassVar["Dimension"]
