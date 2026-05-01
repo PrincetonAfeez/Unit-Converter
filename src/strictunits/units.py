454ac8a4b30c18c1aef2dc3d91ed9c15189de129
Princeton Afeez <princetonafeez@gmail.com>
2026-05-01 13:31:38 -0700

    Units functionality for the strictunits unit converter

---
+++ src/strictunits/units.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from decimal import Decimal
+
+from .dimensions import Dimension
+from .exceptions import ConformabilityError, UnitError
+
