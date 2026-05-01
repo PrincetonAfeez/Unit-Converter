a7c377ce02accc7314357f99eb8b2f6656aa9df8
Princeton Afeez <princetonafeez@gmail.com>
2026-05-01 13:21:44 -0700

    Dimensions functionality for the strictunits unit converter

---
+++ src/strictunits/dimensions.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import ClassVar, Iterable
+
+
+BASE_DIMENSION_COUNT = 7
+BASE_DIMENSION_LABELS = ("L", "M", "T", "I", "Theta", "N", "J")
+
